7324ccb0a3c879358f266e0a410c8a01abbff6fc
Denise Marielle Menguita <denisemenguita@protonmail.com>
2024-10-17 14:11:14 +0800

    added exercise instructions

--- Exercise 01/Coding is Cool.py
+++ Exercise 01/Coding is Cool.py
@@ -1,6 +1,16 @@
 #Exercise 1 - Coding is Cool
 #Denise Marielle Menguita
 
+'''
+Fill in the blanks in the Python code below to output the phrase "Coding is Cool" to the console using variables and string concatenation.
+word1 = 
+word2 = 
+word3 =
+
+# Use string concatenation to combine the variables and print the phrase
+_____(_________)
+'''
+
 word1 = "tea " #variables
 word2 = "coffee "
 word3 = "juice"
--- Exercise 02/Simple Sums.py
+++ Exercise 02/Simple Sums.py
@@ -1,6 +1,16 @@
 #Exercise 2 - Simple Sums
 #Denise Marielle Menguita
 
+'''
+In this exercise, you will create and work with integer variables, perform arithmetic operations, and print the result to the console.
+
+Steps:
+Declare a variable and initialize it with the integer value 8.
+Declare a second variable and initialize it with the integer value 10.
+Declare a third variable that stores the sum of first two numbers.
+Print the value of the sum to the console.
+'''
+
 num1 = 8
 num2 = 10
 sum = num1 + num2
