17d0212943cbdea715402f3fc0f20808247ec6ee
Denise Marielle Menguita <denisemenguita@protonmail.com>
2024-11-07 14:38:36 +0800

    Biography
    made and used proper dictionaries

--- Exercise 03/Biography.py
+++ Exercise 03/Biography.py
@@ -5,18 +5,27 @@
 In this exercise, you'll create a program that stores and prints your name, hometown, and age to the console using a Python dictionary.
 
 Steps:
-Store the information (name, hometown, and age) as key-value pairs in a dictionary.
-Print the values on separate lines using a single print() statement.
-Use variables with appropriate data types for each piece of information.
+1) Store the information (name, hometown, and age) as key-value pairs in a dictionary.
+2) Print the values on separate lines using a single print() statement.
+3) Use variables with appropriate data types for each piece of information.
+
 Advanced Requirements:
-Have the user input their name, hometown, and age instead of hardcoding the values. Try giving both your first and second name when asked for your name. What happens? How can you handle multiple words in Python? Test the program by entering a string value for age (e.g., "twenty"). What happens? How can you prevent this issue?
+Have the user input their name, hometown, and age instead of hardcoding the values.
+Try giving both your first and second name when asked for your name.
+What happens? How can you handle multiple words in Python? Test the program by entering a string value for age (e.g., "twenty").
+What happens? How can you prevent this issue?
 '''
 
-name = "Denise"
-hometown = "Cavite"
-age = 19
+#Create dictionary
+my_bio = {
+        "name" : "Denise",
+        "hometown" : "Cavite",
+        "age" : 19
+}
 
-print("My name is ", name, ".", "I'm from ", hometown, ", and I'm", age)
+print("My name is", my_bio["name"] + "."
+      "\nMy hometown is", my_bio["hometown"] + ",",
+      "\nand I am", my_bio["age"], "years old.")
 
 while True:
         try:
@@ -27,13 +36,7 @@ while True:
                 continue
 print(name2)
 
-while True:
-        try:
-                hometown2 = (input("Where are you from?")) #get input from user
-                break
-        except ValueError:
-                print("Please input letters only...")  
-                continue
+hometown2 = (input("Where are you from?")) #get input from user
 print(hometown2)
 
 while True:
@@ -44,3 +47,11 @@ while True:
                 print("Please input a number...")  
                 continue
 print(age2)
+
+user_bio = {
+        "name": name2,
+        "hometown": hometown2,
+        "age": age2
+}
+
+print("You are", user_bio)
